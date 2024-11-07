aa2fc6cf01725218ce1ed0bb4033347752a00ad2
Valerius <valerius.schmidt@qytera.de>
2024-11-07 14:19:57 +0100

    added first testcases

--- tag-1/playwright/tests/exercise1.spec.ts
+++ tag-1/playwright/tests/exercise1.spec.ts
@@ -1,7 +1,14 @@
-import { test, expect, chromium } from '@playwright/test';
+import { expect, test } from "@playwright/test";
 
-test('tests', async ({ page }) => {
-    await chromium.launch({ headless: false, slowMo: 100 });
-    await page.goto('http://localhost:5173/');
-    await page.getByLabel('Ice Cream').check();
-  });
+test("tests", async ({ page }) => {
+  await page.goto("http://localhost:5173/");
+  await page.getByLabel("Ice Cream").check();
+  await expect(page.getByLabel("Ice Cream")).toBeChecked();
+});
+
+test("testNumber2", async ({ page }) => {
+  await page.goto("http://localhost:5173/");
+  const iceCreamLabel = page.getByLabel("Ice Cream");
+  await iceCreamLabel.check();
+  await expect(iceCreamLabel).toBeChecked();
+});
