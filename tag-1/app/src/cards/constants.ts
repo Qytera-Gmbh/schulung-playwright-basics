891ee3d4a539e51286ef03c66d28e42863f8a673
Sebastian Vollbrecht <sebastian.vollbrecht@qytera.de>
2024-11-08 11:40:23 +0100

    Update caterer mails

--- tag-1/app/src/cards/constants.ts
+++ tag-1/app/src/cards/constants.ts
@@ -14,21 +14,21 @@ export const CATERERS = [
   {
     avatar: "/avatars/platter-perfectionists.svg",
     name: "Platter Perfectionists",
-    email: "platter-perfectionists@example.com",
+    email: "platters@example.com",
     description:
       "We specialize in crafting beautifully curated platters that turn every meal into a memorable experience. From mouth-watering appetizers to decadent desserts, our goal is to bring flavor, flair, and a touch of perfection to your table!",
   },
   {
     avatar: "/avatars/munch-masters.svg",
     name: "Munch Masters",
-    email: "munch-masters@example.com",
+    email: "masters@example.com",
     description:
       "Your go-to team for delicious bites and unforgettable feasts. With flavors that satisfy and presentations that impress, we\u2019re here to make every munch a moment to remember!",
   },
   {
     avatar: "/avatars/banana-buffets.svg",
     name: "Banana Buffets",
-    email: "banana-buffets@example.com",
+    email: "banana@example.com",
     description:
       "We bring fun, flavor, and a touch of the unexpected to every spread, with dishes that are as lively as they are delicious. Whether you\u2019re after tropical vibes or classic comfort, we\u2019ve got you covered with a buffet that\u2019s truly a-peel-ing!",
   },
