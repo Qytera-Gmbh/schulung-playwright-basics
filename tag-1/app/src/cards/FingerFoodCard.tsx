7287949e6e68635182d74188bc5f087c290a78e1
Valerius <valerius.schmidt@qytera.de>
2024-11-11 10:10:07 +0100

    data-testid für finger food checkboxes

--- tag-1/app/src/cards/FingerFoodCard.tsx
+++ tag-1/app/src/cards/FingerFoodCard.tsx
@@ -18,13 +18,24 @@ export default function FingerFoodCard({ date }: { date: DateValue }) {
       </Box>
       <Box mt="md">
         <Group justify="space-between" grow>
-          <Checkbox defaultChecked={true} label="Salad" description="Tomato, Lettuce, Cucumber" />
-          <Checkbox defaultChecked={false} label="Pizza" description="Onions, Pepperoni, Garlic" />
+          <Checkbox
+            defaultChecked={true}
+            label="Salad"
+            description="Tomato, Lettuce, Cucumber"
+            data-testid="checkbox-salad"
+          />
+          <Checkbox
+            defaultChecked={false}
+            label="Pizza"
+            description="Onions, Pepperoni, Garlic"
+            data-testid="checkbox-pizza"
+          />
           <Checkbox
             defaultChecked
             disabled={date?.getDay() !== 0 && date?.getDay() !== 6}
             label="Beer"
             description={`Only available on weekends. Selected day: ${dayString}`}
+            data-testid="checkbox-beer"
           />
         </Group>
         <Space h="lg" />
@@ -39,11 +50,13 @@ export default function FingerFoodCard({ date }: { date: DateValue }) {
             defaultChecked={false}
             label="Chicken Wings"
             description="Spicy, Sweet & Sour"
+            data-testid="checkbox-chickenwings"
           />
           <Checkbox
             defaultChecked={false}
             label="Veggie Sticks"
             description="Grilled Pepper, Mozzarella"
+            data-testid="veggiesticks"
           />
         </Group>
       </Box>
