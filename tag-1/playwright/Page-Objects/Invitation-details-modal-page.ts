832db56b673f0b78efcec228035450e14604a71b
Valerius <valerius.schmidt@qytera.de>
2024-11-11 17:37:54 +0100

    Methoden für invitation details modal page

---
+++ tag-1/playwright/Page-Objects/Invitation-details-modal-page.ts
@@ -0,0 +1,35 @@
+import { Page } from "@playwright/test";
+
+export class TitlePage {
+  constructor(public page: Page) {
+    this.page = page;
+  }
+
+  hostName() {
+    return this.page.getByTestId("name-host");
+  }
+
+  hostMail() {
+    return this.page.getByTestId("mail-host");
+  }
+
+  whenWhereCity() {
+    return this.page.getByTestId("whenwhere-city");
+  }
+
+  whenWhereStreet() {
+    return this.page.getByTestId("whenwhere-street");
+  }
+
+  whenWhereStreetNumber() {
+    return this.page.getByTestId("whenwhere-street-number");
+  }
+
+  dateTimeLocalDate() {
+    return this.page.getByTestId("date-time-local-date");
+  }
+
+  dateTimeLocalTime() {
+    return this.page.getByTestId("date-time-local-time");
+  }
+}
