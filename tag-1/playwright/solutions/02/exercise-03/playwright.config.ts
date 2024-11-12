59504aaac8fd1ef0cb4f7755b79b02c9d0e4ecc4
Sebastian Vollbrecht <sebastian.vollbrecht@qytera.de>
2024-11-12 11:50:08 +0100

    Add solution config

---
+++ tag-1/playwright/solutions/02/exercise-03/playwright.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig, devices } from "@playwright/test";
+
+/**
+ * See https://playwright.dev/docs/test-configuration.
+ */
+export default defineConfig({
+  testDir: "./tests",
+  fullyParallel: true,
+  retries: 0,
+  reporter: [["line"], ["html"]],
+  use: {
+    trace: "retain-on-failure",
+  },
+  projects: [
+    {
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
+    },
+  ],
+});
