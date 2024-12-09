eb6b3d6f34bdce1ed2e36bdf4f5d961c7bd40889
Sebastian Vollbrecht <sebastian.vollbrecht@qytera.de>
2024-12-09 10:36:23 +0100

    Add first test template

--- tag-1/playwright/tests/02/exercise-01/first-tests.spec.ts
+++ tag-1/playwright/tests/02/exercise-01/first-tests.spec.ts
@@ -1,11 +1,11 @@
 import { expect, test } from "@playwright/test";
 
-// =============================================================================================================== //
+// ========================================================================================================================= //
 // Hilfreiche URLs:
-// - Ersten Test schreiben: https://playwright.dev/docs/writing-tests#first-test
+// - Ersten Test schreiben:        https://playwright.dev/docs/writing-tests#first-test
 // - Location anhand von Test IDs: https://playwright.dev/docs/locators#locate-by-test-id
-// - Value überprüfen: https://playwright.dev/docs/api/class-locatorassertions#locator-assertions-to-have-value
-// =============================================================================================================== //
+// - Value überprüfen:             https://playwright.dev/docs/api/class-locatorassertions#locator-assertions-to-have-value
+// ========================================================================================================================= //
 
 test("First test", async ({ page }) => {
   // TODO...
